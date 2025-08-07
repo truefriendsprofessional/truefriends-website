dfd79ec3dfcb4c1f0f490404f78c68024ec2498b
harmanpreet7916 <harmanpreetgirn@gmail.com>
2025-08-08 02:40:06 +0530

    changed bg transition on why us

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -26,7 +26,7 @@ const WhyUs = () => {
     <section
       id="why-us"
       ref={sectionRef}
-      className={`w-full h-screen flex flex-col justify-center items-center md:items-end px-4 sm:px-6 md:px-12 py-10 sm:py-14 md:py-20 text-center md:text-right min-h-[60vh] transition-all duration-2000 font-[Poppins,sans-serif] ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-16'}`}
+      className="w-full h-screen flex flex-col justify-center items-center md:items-end px-4 sm:px-6 md:px-12 py-10 sm:py-14 md:py-20 text-center md:text-right min-h-[60vh] font-[Poppins,sans-serif]"
       style={{
         fontFamily: "'Roboto','Poppins', 'Segoe UI', Arial, sans-serif",
         backgroundImage: `url(${sleepingImg})`,
@@ -36,8 +36,7 @@ const WhyUs = () => {
       }}
     >
 
-
-      <h2 className="text-6xl sm:text-7xl md:text-6xl lg:text-8xl font-extrabold mb-6 text-transparent bg-clip-text bg-gradient-to-r to-[#35170B] from-[#EEC970] drop-shadow-lg tracking-tight w-full sm:w-4/5 md:w-1/2 lg:w-1/2 mx-auto md:mr-0 md:mx-0">
+      <h2 className={`text-6xl sm:text-7xl md:text-6xl lg:text-8xl font-extrabold mb-6 text-transparent bg-clip-text bg-gradient-to-r to-[#35170B] from-[#EEC970] drop-shadow-lg tracking-tight w-full sm:w-4/5 md:w-1/2 lg:w-1/2 mx-auto md:mr-0 md:mx-0 transition-all duration-2000 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-16'}`}>
         Treatment that 
         <br />
         <FlipWords words={words} /> <br />
